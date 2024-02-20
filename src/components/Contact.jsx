e9d526f8e5018eb9025ff50b6abd15fa8f7bec10
Vishal Dahiya <vishaldahiya12@gmail.com>
2024-02-20 21:14:59 +0530

    Contact and Projects pages added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="text-center justify-center py-20 space-y-4 text-4xl">
+    <div className="text-center justify-center py-20 space-y-4 text-4xl font-bold">
       <h1>Contact</h1>
       <p>Comming Soon.......</p>
     </div>
